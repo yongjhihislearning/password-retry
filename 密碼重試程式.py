6dfdf8232e75cc9499fb7680e6afa82a402af2fd
Yongjhih Lin <yongjhih85@gmail.com>
2020-11-28 11:06:44 +0800

    basic version done

--- 密碼重試程式.py
+++ 密碼重試程式.py
@@ -3,14 +3,14 @@
 # 讓使用者[最多輸入3次密碼]
 # 不對的話，就印出"密碼錯誤! 還有＿次機會"
 # 對的話，就印出"登入成功!"
-i = 2
+i = 3
 while True:
     password = input('請輸入密碼: ')
     if password == 'a123456':
          print('登入成功')
          break
-    elif i == 0:
-         break
     else:
-    	print('密碼錯誤! 還有', i, '次機會')
     	i = i - 1
+    	print('密碼錯誤! 還有', i, '次機會')
+    	if i == 0:
+    	     break
