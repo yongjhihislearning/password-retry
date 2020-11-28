7aa74ef36c79539cc0cb57fb885995f3a778eec3
Yongjhih Lin <yongjhih85@gmail.com>
2020-11-28 11:13:41 +0800

    change to while i > 0

--- 密碼重試程式.py
+++ 密碼重試程式.py
@@ -4,7 +4,7 @@
 # 不對的話，就印出"密碼錯誤! 還有＿次機會"
 # 對的話，就印出"登入成功!"
 i = 3
-while True:
+while i > 0:
     password = input('請輸入密碼: ')
     if password == 'a123456':
          print('登入成功')
@@ -12,5 +12,3 @@ while True:
     else:
     	i = i - 1
     	print('密碼錯誤! 還有', i, '次機會')
-    	if i == 0:
-    	     break
